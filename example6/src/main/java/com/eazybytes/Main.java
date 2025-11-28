8e5c99731227d58c30f3db9db7ee734cd9ddf25c
Tejveer LNU <sharmakumarpushpendra7@gmail.com>
2025-11-28 19:40:30 +0530

    Adding functionality to the spring bean created with the help of @Component by adding @PostConstruct and @PreDestroy methods

---
+++ example6/src/main/java/com/eazybytes/Main.java
@@ -0,0 +1,15 @@
+package com.eazybytes;
+
+import com.eazybytes.beans.Vehicle;
+import com.eazybytes.config.ProjectConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+        Vehicle veh1 = context.getBean(Vehicle.class);
+        System.out.println("Vehicle name is: "+veh1.getName());
+        veh1.sayHello();
+        context.close();
+    }
+}

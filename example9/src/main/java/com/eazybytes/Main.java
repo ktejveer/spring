6d5498bc42de4d5fb7cb38f37d360cf4dd8826e6
Tejveer LNU <sharmakumarpushpendra7@gmail.com>
2025-11-29 16:59:39 +0530

    Wiring beans using method call in Spring Context

---
+++ example9/src/main/java/com/eazybytes/Main.java
@@ -0,0 +1,20 @@
+package com.eazybytes;
+
+import com.eazybytes.beans.Person;
+import com.eazybytes.beans.Vehicle;
+import com.eazybytes.config.ProjectConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+        Person person = context.getBean(Person.class);
+        System.out.println("Person name is: "+person.getName());
+        System.out.println("Person having: " + person.getVehicle().getName());
+
+        Vehicle vehicle = context.getBean(Vehicle.class);
+        System.out.println("Vehicle name is: "+vehicle.getName());
+    }
+}

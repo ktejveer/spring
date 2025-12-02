673b7cf5d2ad115d1202698fd07d428b07e668c0
Tejveer LNU <sharmakumarpushpendra7@gmail.com>
2025-12-02 09:25:44 +0530

    Sample related to lazy and eager loading

---
+++ example15/src/main/java/com/eazybytes/config/ProjectConfig.java
@@ -0,0 +1,13 @@
+package com.eazybytes.config;
+
+import com.eazybytes.beans.Vehicle;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+@ComponentScan(basePackages = "com.eazybytes.beans")
+public class ProjectConfig {
+
+}

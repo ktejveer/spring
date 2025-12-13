c46a57fd4c766ae14811b3c5d2644ac58dd18271
Tejveer LNU <sharmakumarpushpendra7@gmail.com>
2025-12-14 00:34:21 +0530

    Added some more aspects @AfterReturning and @AfterThrowing

--- example17/src/main/java/com/eazybytes/aspects/LoggerAspect.java
+++ example17/src/main/java/com/eazybytes/aspects/LoggerAspect.java
@@ -1,6 +1,9 @@
 package com.eazybytes.aspects;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.core.annotation.Order;
@@ -8,6 +11,7 @@ import org.springframework.stereotype.Component;
 
 import java.time.Duration;
 import java.time.Instant;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 @Aspect
@@ -25,5 +29,15 @@ public class LoggerAspect {
         long timeElapsed = Duration.between(start, finish).toMillis();
         logger.info("Time took to execute the method : "+timeElapsed);
         logger.info(joinPoint.getSignature().toString() + "method execution end");
+    }
+
+    @AfterThrowing(value = "execution(* com.eazybytes.services.*.*(..))", throwing = "ex")
+    public void logException(JoinPoint joinPoint, Exception ex){
+        logger.log(Level.SEVERE, joinPoint.getSignature() + " An exception thrown with the help of @AfterThrowing which happened due to : "+ex.getMessage());
+    }
+
+    @AfterReturning(value = "execution(* com.example.services.*.*(..))", returning ="retVal")
+    public void logStatus(JoinPoint joinPoint, Object retVal){
+        logger.log(Level.INFO, joinPoint.getSignature() + " method successfully processed with the status "+retVal.toString());
     }
 }
